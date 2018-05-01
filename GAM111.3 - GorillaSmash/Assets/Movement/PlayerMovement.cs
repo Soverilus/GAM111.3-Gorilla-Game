7e376afb97d9c9dbac7dc941a9f366a4a9486a7b
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-05-01 11:37:23 +0800

    changed a whole bunch of shit

--- GAM111.3 - GorillaSmash/Assets/Movement/PlayerMovement.cs
+++ GAM111.3 - GorillaSmash/Assets/Movement/PlayerMovement.cs
@@ -18,6 +18,8 @@ public class PlayerMovement : MonoBehaviour {
     public float groundVel;
     public float airVel;
     public float jumpVel;
+    Vector3 inputActual;
+    Vector3 rawInputActual;
     Rigidbody playerRB;
     enum MoveState {
         OnGround,
@@ -36,24 +38,49 @@ public class PlayerMovement : MonoBehaviour {
         playerRB = GetComponent<Rigidbody>();
     }
 
+    void InputCalculation(float inputY) {
+        inputActual = new Vector3(Camera.main.transform.TransformDirection(input).x, inputY, Camera.main.transform.TransformDirection(input).z).normalized;
+        rawInputActual = new Vector3(Camera.main.transform.TransformDirection(rawInput).x, inputY, Camera.main.transform.TransformDirection(rawInput).z).normalized;
+        Debug.Log(rawInputActual);
+    }
+
     void FixedUpdate() {
         if (!canJump) gorrilaAnim.SetBool("Running", false);
         if (Input.GetAxisRaw("Jump") == 0) {
             canButtonJump = true;
         }
+        input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        rawInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         RaycastHit hit;
-        if (Physics.SphereCast(transform.position, myCapsuleCollider.radius - 1f, Vector3.down, out hit, myCapsuleCollider.height*0.5f + groundedDist) && isJumping == false) {
-            gorrilaAnim.SetBool("Jumped", false);
-            currentMoveState = MoveState.OnGround;
-            canJump = true;
+        if (Physics.SphereCast(transform.position, myCapsuleCollider.radius - 0.1f, Vector3.down, out hit, myCapsuleCollider.height * 0.5f + groundedDist) && isJumping == false) {
+            float dotProd = Vector3.Dot(Vector3.down, hit.normal);
+            //Debug.Log(dotProd);
+            if (dotProd <= -0.7f) {
+                isFalling = true;
+                gorrilaAnim.SetBool("Jumped", false);
+                currentMoveState = MoveState.OnGround;
+                canJump = true;
+                InputCalculation(1f - Mathf.Abs(dotProd));
+                Debug.Log(1f - Mathf.Abs(dotProd));
+            }
+            else if (isJumping) {
+                currentMoveState = MoveState.OnJump;
+                InputCalculation(0f);
+            }
+            if (dotProd > -0.7f) {
+                canJump = false;
+                playerRB.AddForce(hit.normal.x * 1000, 100f, hit.normal.z * 1000);
+            }
         }
         else if (isJumping) {
             currentMoveState = MoveState.OnJump;
+            InputCalculation(0f);
         }
-        else currentMoveState = MoveState.InAir;
-
-        input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        rawInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        else {
+            currentMoveState = MoveState.InAir;
+            InputCalculation(0f);
+        }
+        
 
         switch (currentMoveState) {
             case MoveState.OnGround:
@@ -68,16 +95,17 @@ public class PlayerMovement : MonoBehaviour {
                 break;
         }
         if (rawInput.magnitude != 0) {
-            Vector3 facingDir = input.normalized;
+            Vector3 facingDir = inputActual;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(facingDir, Vector3.up), Time.deltaTime * 5);
             transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
         }
     }
 
     void OnGroundMove() {
-        MoveGorilla(groundVel);
+        //MoveGorilla(groundVel);
         if (rawInput.magnitude != 0) {
             gorrilaAnim.SetBool("Running", true);
+            MoveGorilla(groundVel);
         }
         else if (rawInput.magnitude == 0) {
             gorrilaAnim.SetBool("Running", false);
@@ -110,13 +138,13 @@ public class PlayerMovement : MonoBehaviour {
 
     void MoveGorilla(float multVel) {
         if (playerRB.velocity.magnitude < maxVel) {
-            playerRB.AddForce((playerRB.mass + speed * 2) * multVel * rawInput);
+            playerRB.AddForce((playerRB.mass + speed * 2) * multVel * rawInputActual);
         }
         else {
             Vector3.ClampMagnitude(playerRB.velocity, maxVel);
         }
         if (canButtonJump)
-        playerRB.velocity = Vector3.Lerp(playerRB.velocity, new Vector3(0, playerRB.velocity.y, 0), 0.075f);
+            playerRB.velocity = Vector3.Lerp(playerRB.velocity, new Vector3(0, playerRB.velocity.y, 0), 0.075f);
     }
     void FallTimer() {
         isJumping = false;

0994ec78f12769045b9972a4856b569eb079eed5
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-05-03 11:53:44 +0800

    Holy fucking shit this fucking camera fucking hell goddamnit fuck

--- GAM111.3 - GorillaSmash/Assets/Camera/PlayerToCam.cs
+++ GAM111.3 - GorillaSmash/Assets/Camera/PlayerToCam.cs
@@ -30,11 +30,12 @@ public class PlayerToCam : MonoBehaviour {
         }
 
         //cameraObj.transform.position = transform.position + ((cameraObj.transform.position - transform.position).normalized * followMax);
-        //Vector3 refRef = Vector3.zero;
-        Quaternion targetRotation = Quaternion.LookRotation(transform.position - cameraObj.transform.position);
 
-        cameraObj.transform.rotation = Quaternion.Slerp(cameraObj.transform.rotation, targetRotation, camFol.smoothTime*5f * Time.deltaTime);
-        //cameraObj.transform.LookAt(Vector3.SmoothDamp(cameraObj.transform.position, transform.position, ref refRef, camFol.smoothTime));
+        //Quaternion targetRotation = Quaternion.LookRotation(transform.position - cameraObj.transform.position);
+        //cameraObj.transform.rotation = Quaternion.Slerp(cameraObj.transform.rotation, targetRotation, camFol.smoothTime*5f * Time.deltaTime);
+
+		Vector3 refRef = Vector3.zero;
+        cameraObj.transform.LookAt(Vector3.SmoothDamp(cameraObj.transform.position, transform.position, ref refRef, camFol.smoothTime));
 
     }
 }
--- GAM111.3 - GorillaSmash/Assets/Camera/SmoothFollow.cs
+++ GAM111.3 - GorillaSmash/Assets/Camera/SmoothFollow.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SmoothFollow : MonoBehaviour {
-    [SerializeField]
-    GameObject upCamObj;
-    [SerializeField]
-    GameObject leftCamObj;
     Vector3 positionHolder;
     GameObject player;
     Vector3 trueInput;
@@ -15,34 +11,42 @@ public class SmoothFollow : MonoBehaviour {
     public float smoothTime;
     Vector3 crossProdInput;
     public float camSensitivity;
-    Vector3 heightAbove;
+	Vector3 movementVector;
+	Vector3 crossProd;
 
     void Start() {
-        heightAbove = Vector3.zero;
+		movementVector = Vector3.zero;
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update() {
-        if (player != null) {
-            input = new Vector3(Input.GetAxis("CamHorizontal"), Input.GetAxis("CamVertical"), 0f);
-            rawInput = new Vector3(Input.GetAxisRaw("CamHorizontal"), Input.GetAxisRaw("CamVertical"), 0f);
+		if (player != null) {
+			input = new Vector3 (Input.GetAxis ("CamHorizontal"), Input.GetAxis ("CamVertical"), 0f);
+			rawInput = new Vector3 (Input.GetAxisRaw ("CamHorizontal"), Input.GetAxisRaw ("CamVertical"), 0f);
 
-            if (rawInput.magnitude != 0f) {
-
-            }
-        }
-    }
+			if (rawInput.magnitude != 0f) {
+				if ((Vector3.Dot (Vector3.up, -transform.forward) >= 0.8f && rawInput.y >= 0) || 
+					(Vector3.Dot (Vector3.up, -transform.forward) <= -0.8f && rawInput.y <= 0)||
+					(Vector3.Dot (Vector3.up, -transform.forward) < 0.8f && Vector3.Dot (Vector3.up, -transform.forward) > -0.8f)) {
+					movementVector = (transform.up * -input.y + transform.right * -input.x);
+				}
+			}
+		}
+	}
     public void SmoothCam(Vector3 camVect) {
         if (player != null) {
             Vector3 refRef = Vector3.zero;
+
+
             //if (input.magnitude > 0) {
             //positionHolder = (camVect - trueInput);
             //}
             //else {
-            positionHolder = camVect + heightAbove;
+			positionHolder = camVect + movementVector.normalized * camSensitivity;
             //}
             transform.position = Vector3.SmoothDamp(transform.position, positionHolder, ref refRef, smoothTime);
             //transform.position = positionHolder;
+			movementVector = Vector3.zero;
         }
     }
 }
